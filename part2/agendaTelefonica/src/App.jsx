936494c052edd595596e3f1066f1c7f3e2c35b45
EdgarAlvarado24 <edgar.alvarado.16@gmail.com>
2024-09-10 20:58:38 +0000

    ejercicio 2.1

--- part2/agendaTelefonica/src/App.jsx
+++ part2/agendaTelefonica/src/App.jsx
@@ -6,19 +6,54 @@ const App = () => {
   ]) 
   const [newName, setNewName] = useState('')
 
+  const addPerson = (event) =>{
+    event.preventDefault()
+
+    const personObject = {
+      name: newName,
+    }
+
+    const personsExist = (person) => person.name === newName
+    
+    if (persons.some(personsExist)){
+      alert(`${newName} is already added to phonebook`)
+    }else{
+      setPersons(persons.concat(personObject))
+    }
+
+    setNewName('')
+  }
+
+  const handlePersonChange = (event) =>{
+    console.log(event.target.value)
+    
+
+      
+      setNewName(event.target.value)
+
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={addPerson}>
         <div>
-          name: <input />
+          name: <input  value={newName} onChange={handlePersonChange}/>
         </div>
+        <div>debug: {newName}</div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+      <div>
+          {
+            persons.map((person)=> 
+              <h3 key={person.id}>
+                {person.name}
+              </h3>
+          )}
+      </div>
     </div>
   )
 }
