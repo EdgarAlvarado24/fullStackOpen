892d0da48e91e2db667cd1d07af4c1c248a2544d
EdgarAlvarado24 <edgar.alvarado.16@gmail.com>
2024-10-12 16:49:38 -0400

    modified

--- part3/server-agenda/index.js
+++ part3/server-agenda/index.js
@@ -1,8 +1,7 @@
 const express = require('express')
 const fs = require('fs')
 const app = express()
-const PORT = 3001
-const baseUri = 'http://localhost:3001/api/persons'
+const PORT = process.env.PORT|| 3001
 
 app.use(express.json())
 
