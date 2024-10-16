5b2c101066c093faa5fce4a467984973a4af57e5
EdgarAlvarado24 <edgar.alvarado.16@gmail.com>
2024-10-16 03:07:30 -0400

    chagned to use database and handleErrors in notes

--- part3/server-notes/index.js
+++ part3/server-notes/index.js
@@ -1,10 +1,12 @@
+require('dotenv').config()
 const express = require('express')
 const fs = require('fs')
 const  bodyParser = require('body-parser')
 const cors = require('cors')
+const Note = require('./models/note')
 
 const app = express()
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 
 app.use(bodyParser.json());
 app.use(cors())
@@ -35,49 +37,77 @@ app.get('/', (req, res)=>{
   res.send('Welcome to my fisrt Api Rest')
 })
 
-app.get('/api/notes', (req, res)=>{
-  const data = readData()
-  res.send(data.notes)
+app.get('/api/notes', (request, response) => {
+  Note.find({}).then(notes => {
+    response.json(notes)
+  })
 })
 
-app.get('/api/notes/:id', (req, res)=>{
-  const data = readData()
-  const id = req.params.id
-  const note = data.notes.filter(note => note.id == id ? note : '')
-  res.send(note)
+app.get('/api/notes/:id', (request, response)=>{
+  Note.findById(request.params.id).then(note =>{
+    response.json(note)
+  })
 })
 
-app.post('/api/notes', (req, res) =>{
-  const data = readData();
-  const body = req.body;
-  const newNote = {
-    id: data.notes.length + 1,
-    ...body
-  };
-  data.notes.push(newNote);
-  writeData(data)
-  console.log(newNote)
-  res.json(newNote)
+app.post('/api/notes', (request, response) =>{
+  const body = request.body;
+
+  if(body.content === undefined){
+    return response.status(400).json({error: 'content missing'})
+  }
+
+  const note = new Note({
+    content: body.content,
+    important: body.important || false,
+  })
+
+  note.save()
+  .then(savedNote =>{
+    response.json(savedNote)
+  })
+  .catch(error => next(error))
 })
 
-app.put('/api/notes/:id', (req, res) =>{
-  const data = readData();
-  const body = req.body;
-  const id = Number(req.params.id)
-  const noteIndex = data.persons.findIndex((note) => note.id === id)
-  data.notes[noteIndex] = {
-    ...data.notes[noteIndex],
-    ...body
-  };
-  writeData(data);
-  res.json({message: "note updated successfully"})
+app.put('/api/notes/:id', (request, response) =>{
+  const {content, important} = request.body
+
+  Note.findByIdAndUpdate(
+    request.params.id,
+    {content, important},
+    {new: true, runValidators: true, context: 'query'}
+  )
+  .then(updatedNote => {
+    response.json(updatedNote)
+  })
+  .catch(error => next(error))
 })
 
-app.delete('/api/notes/:id', (req, res) =>{
-  const data = readData();
-  const id = Number(req.params.id)
-  const noteIndex = data.notes.findIndex((note) => note.id == id)
-  data.notes.splice(noteIndex,1);
-  writeData(data)
-  res.json({message: "note deleted successfully"})
+app.delete('/api/notes/:id', (request, response) =>{
+  Note.findByIdAndDelete(request.params.id)
+    .then(result =>{
+      response.status(204).end()
+    })
+    .catch(error => next(error))
 })
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+// controlador de solicitudes con endpoint desconocido
+app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } else if(error.name === 'ValidationError'){
+    return response.status(400).json({error: error.message})
+  }
+
+  next(error)
+}
+
+// este debe ser el último middleware cargado, ¡también todas las rutas deben ser registrada antes que esto!
+app.use(errorHandler)
