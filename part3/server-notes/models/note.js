c54a5173ecbaa5346eb662381c0209474a7f1171
EdgarAlvarado24 <edgar.alvarado.16@gmail.com>
2024-10-17 04:31:50 -0400

    restructure server-notes

--- part3/server-notes/models/note.js
+++ part3/server-notes/models/note.js
@@ -1,29 +1,13 @@
-require('dotenv').config();
 const mongoose = require('mongoose')
-
-mongoose.set('strictQuery',false)
-
-const url = process.env.MONGODB_URI
-
-console.log('connecting to', url)
-
-mongoose.connect(url)
-.then(() => 
-    console.log('Connected to MongoDB Atlas!'))
-.catch(error => 
-    console.error('Error connecting to MongoDB Atlas:', error.message
-));
 
 const noteSchema = new mongoose.Schema({
   content: {
     type: String,
-    minLength: 5,
-    required: true
+    required: true,
+    minLength: 5
   },
   important: Boolean,
 })
-
-const Note = mongoose.model('Note', noteSchema)
 
 noteSchema.set('toJSON', {
   transform: (document, returnedObject) => {
